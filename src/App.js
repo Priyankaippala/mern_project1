307eeb8a88adda0d74d79dc7a214b5514b0a1881
Priyanka Ippala <priyankaippala2003@gmail.com>
2024-07-10 21:33:32 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Student from './pages/student';
+import {MyContext} from './context/MyContext';
+import { useState } from 'react';
 
 function App() {
+  const [loggedIn,setLoggedIn] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MyContext.Provider value={{loggedIn,setLoggedIn}}>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/students/*" element={<Student />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+    </MyContext.Provider>
   );
 }
 
